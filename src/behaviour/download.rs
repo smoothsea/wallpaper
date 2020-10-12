967d614bc6d3fe2ce2c50193eb3e370b39866b88
chenpz <c@qq.com>
2020-10-12 12:31:53 +0800

    first commit

---
+++ src/behaviour/download.rs
@@ -0,0 +1,143 @@
+use crate::Params;
+use crate::fatal;
+use regex::Regex;
+use reqwest;
+use std::error::Error;
+use std::fs::*;
+use std::io::{Read, Write};
+use std::path::Path;
+
+#[derive(Debug)]
+struct Pic {
+    filename: String,
+    body: Vec<u8>,
+}
+
+impl Pic {
+    fn new(filename: String, body: Vec<u8>) -> Pic {
+        Pic {
+            filename: filename,
+            body: body,
+        }
+    }
+}
+
+pub fn download(params: &Params) {
+    let resolution = params
+        .download_resolution
+        .clone()
+        .expect("下载图片分辨率错误");
+    let empty_dir = params.download_empty;
+    let sfw = params.download_sfw;
+
+    let dir = Path::new(&params.dir);
+    if !dir.is_dir() {
+        fatal!("{} is not a dir", dir.to_string_lossy());
+    }
+
+    let pics;
+    match get_pics(&resolution, sfw) {
+        Ok(ret) => pics = ret,
+        Err(e) => panic!("main error {}", e),
+    }
+    println!("已爬取{}张壁纸", pics.len());
+
+    if pics.len() > 0 {
+        if empty_dir {
+            //del previous files
+            for entry in dir.read_dir().expect(&format!("{} can't read", params.dir)) {
+                if let Ok(entry) = entry {
+                    if let Err(_e) = remove_file(entry.path()){
+                        fatal!("{} is failed remove",entry.path().to_string_lossy());
+                    }
+                }
+            }
+        }
+    }
+
+    for pic in pics.iter() {
+        let file_name = format!("{}{}", params.dir, pic.filename);
+        let path = Path::new(&file_name);
+
+        let mut file = match File::create(path) {
+            Err(why) => {
+                fatal!("couldn't create {}: {}", file_name, why.to_string());
+            },
+            Ok(file) => file,
+        };
+
+        match file.write_all(&pic.body) {
+            Err(why) => {
+                fatal!("couldn't write to {}:{}", file_name, why.to_string());
+            },
+            Ok(_) => {}
+        }
+    }
+}
+
+fn get_pics(resolution: &str, sfw: bool) -> Result<Vec<Pic>, Box<dyn Error>> {
+    let mut category = 111;
+    let mut purity = 110;
+    if sfw {
+        category = 110;
+        purity = 100;
+    }
+
+    let url = format!(
+        "{}{}{}{}{}{}{}",
+        "https://wallhaven.cc/search?categories=",
+        category,
+        "&purity=",
+        purity,
+        "&atleast=",
+        resolution,
+        "&sorting=random&order=desc"
+    );
+    let client = reqwest::Client::new();
+    let mut res = client.get(&url).send().expect("an error");
+    let mut body = "".to_string();
+
+    res.read_to_string(&mut body)?;
+
+    let re = Regex::new("class=\"preview\"\\s+href=\"(.*?)\"")?;
+    let mut pics = Vec::new();
+    for caps in re.captures_iter(&body) {
+        match get_pic_from_detail_page_url(&caps[1]) {
+            Ok(pic) => {
+                pics.push(pic);
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    }
+
+    Ok(pics)
+}
+
+fn get_pic_from_detail_page_url(url: &str) -> Result<Pic, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    let mut res = client.get(url).send()?;
+    let mut body = "".to_string();
+
+    res.read_to_string(&mut body)?;
+
+    let re = Regex::new("id=\"wallpaper\" src=\"(.*?)\"")?;
+    let mut full_pic_url = "".to_string();
+    for caps in re.captures_iter(&body) {
+        full_pic_url = caps[1].to_string();
+    }
+
+    let filename = get_basename(&full_pic_url);
+
+    let mut res = client.get(&full_pic_url).send()?;
+    let mut body = Vec::new();
+    res.read_to_end(&mut body)?;
+
+    Ok(Pic::new(filename, body))
+}
+
+fn get_basename(url: &str) -> String {
+    let pieces = url.split("/");
+    pieces.last().unwrap().to_string()
+}
---
+++ src/main.rs
@@ -0,0 +1,340 @@
+mod behaviour;
+
+use clap::{App, Arg, SubCommand};
+use rand::{Rng};
+use std::env;
+use std::error::Error;
+use std::process::{Command, Stdio};
+use std::{fs, thread, time};
+
+use crate::behaviour::download::{download};
+
+#[macro_export]
+macro_rules! fatal {
+    ($($tt: tt)*) => {
+        use std::io::Write;
+        writeln!(&mut ::std::io::stderr(), $($tt)*).unwrap();        
+        ::std::process::exit(1);
+    };
+}
+
+#[derive(Debug, Clone)]
+pub struct Params {
+    dir: String,
+    is_video: bool,
+    is_download: bool,
+    video_file: Option<String>,
+    video_compress_dir: Option<String>,
+    download_empty: bool,
+    download_resolution: Option<String>,
+    download_sfw: bool
+}
+
+impl Params {
+    fn new(
+        dir: String,
+        is_video: bool,
+        video_file: Option<String>,
+        is_download: bool,
+        video_compress_dir: Option<String>,
+        download_empty: bool,
+        download_resolution: Option<String>,
+        download_sfw: bool
+    ) -> Params {
+        Params {
+            dir,
+            is_video,
+            is_download,
+            video_file,
+            video_compress_dir,
+            download_empty,
+            download_resolution,
+            download_sfw
+        }
+    }
+}
+
+fn main() {
+    let params:Params = match get_params() {
+        Ok(p) => p,
+        Err(e) => {
+            fatal!("{}", e);
+        }
+    };
+    check_dependency(&params);
+
+    handle_exit(params.clone());
+
+    if params.is_download {
+        download(&params);
+        std::process::exit(1);
+    }
+
+    loop {
+        if params.is_video {
+            video(&params);
+        } else {
+            image(&params);
+        }
+    }
+}
+
+fn video(params: &Params) {
+    let interval = time::Duration::from_millis(1);
+    let dir = params.video_compress_dir.clone().unwrap();
+    fs::create_dir(&dir).unwrap();
+    println!("开始转换视频...");
+    Command::new("ffmpeg")
+        .arg("-y")
+        .arg("-ss")
+        .arg("00:00")
+        .arg("-i")
+        .arg(params.video_file.clone().unwrap())
+        .arg(format!("{}/filename%09d.jpg", &dir))
+        .output()
+        .expect("sh command failed to start");
+
+    let mut v: Vec<_> = fs::read_dir(&dir)
+        .unwrap()
+        .map(|r| r.unwrap())
+        .collect();
+    v.sort_by_key(|dir| dir.path());
+    let count = v.len();
+
+    let mut i = 0;
+    while i < count {
+        let pic_path = format!("{}", &v[i].path().display());
+        let mut command = "/usr/bin/feh --bg-scale ".to_string();
+        command.push_str(&pic_path);
+        Command::new("sh")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("faild");
+
+        i += 1;
+        if i == count - 1 {
+            i = 0;
+        }
+
+        thread::sleep(interval);
+    }
+}
+
+fn image(params: &Params) {
+    let ten_millis = time::Duration::from_millis(60000);
+    let dir = &params.dir;
+    if let Err(_e) = fs::read_dir(&dir) {
+        fatal!("目录{}不存在或有权限问题", &dir);
+    }
+
+    let command = format!("feh --bg-scale --randomize {}", params.dir);
+    Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("faild");
+
+    thread::sleep(ten_millis);
+}
+
+fn get_params() -> Result<Params, Box<dyn Error>> {
+    let matches = App::new("壁纸管理")
+        .version("1.0")
+        .help_message("帮助").version_message("版本信息")
+        .author("smoothsea@yeah.net")
+        .about("自动切换，下载壁纸。可以设置视频为壁纸")
+        .arg(
+            Arg::with_name("directory")
+                .short("d")
+                .long("directory")
+                .help("壁纸文件夹")
+                .takes_value(true),
+        )
+        .subcommand(
+            SubCommand::with_name("video").help_message("帮助").version_message("版本信息")
+            .about("设置视频为壁纸").arg(
+                Arg::with_name("file")
+                    .short("f")
+                    .long("file")
+                    .help("视频地址")
+                    .validator(is_mp4)
+                    .required(true)
+                    .takes_value(true),
+            ),
+        )
+        .subcommand(
+            SubCommand::with_name("download").help_message("帮助").version_message("版本信息")
+            .about("下载壁纸").arg(
+                Arg::with_name("empty")
+                .short("e")
+                .long("empty")
+                .help("清空壁纸目录")
+                .empty_values(true),
+            ).arg(
+                Arg::with_name("resolution")
+                .short("r")
+                .long("resolution")
+                .help("设置下载壁纸分辨率")
+                .takes_value(true)
+                .empty_values(false)
+                .required(true)
+            ).arg(
+                Arg::with_name("sfw")
+                .long("sfw")
+                .help("适合上班")
+                .empty_values(true)
+            ).arg(
+                Arg::with_name("directory")
+                .short("d")
+                .long("directory")
+                .help("壁纸文件夹")
+                .takes_value(true)
+            ),
+        )
+        .get_matches();
+    let is_video = matches.is_present("video");
+    let is_download = matches.is_present("download");
+    let mut video_file = None;
+    let mut video_compress_dir = None;
+    if is_video {
+        video_file = Some(
+            matches
+                .subcommand_matches("video")
+                .unwrap()
+                .value_of("file")
+                .unwrap_or("no file")
+                .to_owned(),
+        );
+
+        let rand_string: String = gen_rand_string();
+        let mut path = env::temp_dir();
+        path.push(rand_string);
+        video_compress_dir = Some(path.as_path().to_str().unwrap().to_owned());
+    }
+
+    let mut default_dir = "".to_owned();
+    for (key, val) in env::vars() {
+        if key == "HOME".to_owned() {
+            default_dir = format!("{}{}", default_dir, val);
+        }
+    }
+    default_dir.push_str("/.wallpaper/");
+    let mut download_empty = false;
+    let mut download_sfw = false;
+    let mut download_resolution = None;
+    let mut dir;
+    if is_download {
+        download_empty = matches.subcommand_matches("download")
+                        .unwrap()
+                        .is_present("empty");
+        download_sfw = matches.subcommand_matches("download")
+                        .unwrap()
+                        .is_present("sfw");
+        download_resolution = Some(
+            matches
+                .subcommand_matches("download")
+                .unwrap()
+                .value_of("resolution")
+                .unwrap_or("no resolution")
+                .to_owned(),
+        );
+
+        dir = matches.subcommand_matches("download")
+        .unwrap()
+        .value_of("directory")
+        .unwrap_or(&default_dir)
+        .to_owned();
+    } else {
+       dir = matches
+        .value_of("directory")
+        .unwrap_or(&default_dir)
+        .to_owned();
+    }
+    if !dir.ends_with("/") {
+        dir.push_str("/");
+    }
+
+
+    Ok(Params::new(
+        dir,
+        is_video,
+        video_file,
+        is_download,
+        video_compress_dir,
+        download_empty,
+        download_resolution,
+        download_sfw,
+    ))
+}
+
+fn is_mp4(file: String) -> Result<(), String> {
+    if !file.ends_with(".mp4") {
+        return Err(String::from("视频文件需要mp4格式"));
+    }
+
+    match fs::OpenOptions::new().read(true).open(file) {
+        Ok(_f) => {}
+        Err(e) => {
+            return Err(format!("{}", e));
+        }
+    }
+
+    Ok(())
+}
+
+fn check_dependency(params: &Params) {
+    let mut dependencies: Vec<&str> = vec![];
+
+    if !params.is_download {
+        dependencies.append(&mut vec!["feh"]);
+    }
+
+    if params.is_video {
+        dependencies.append(&mut vec![
+            "ffmpeg", "convert", "xrandr", "xdg-open", "bash", "sed",
+        ]);
+    }
+
+    for i in dependencies.iter() {
+        match Command::new(i)
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+        {
+            Ok(_out) => {}
+            Err(e) => {
+                if let std::io::ErrorKind::NotFound = e.kind() {
+                    fatal!("{} 程序不存在，请先安装它", i);
+                } else {
+                    fatal!("{} 发生错误 :(", i);
+                }
+            }
+        }
+    }
+}
+
+fn gen_rand_string() -> String {
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789";
+    const PASSWORD_LEN: usize = 20;
+    let mut rng = rand::thread_rng();
+
+    (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0, CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect()
+}
+
+fn handle_exit(params: Params) {
+     ctrlc::set_handler(move || {
+         if params.is_video {
+             fs::remove_dir_all(params.video_compress_dir.clone().unwrap()).unwrap();
+         }
+         std::process::abort();
+     })
+     .expect("Error setting Ctrl-C handler");
+}
