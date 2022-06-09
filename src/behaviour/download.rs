43745a15600874622a813cd9d7a8027ecf339d43
smoothsea-zr <1@q.com>
2022-06-09 15:50:59 +0800

    Add wallpaperstock source

--- src/behaviour/download.rs
+++ src/behaviour/download.rs
@@ -1,12 +1,14 @@
-use crate::Params;
 use crate::fatal;
+use crate::Params;
+
+use rand::Rng;
 use regex::Regex;
 use reqwest;
-use std::fs::*;
-use std::path::Path;
-use std::error::Error;
-use std::io::{Read, Write};
 use std::collections::HashMap;
+use std::error::Error;
+use std::fs::*;
+use std::io::{Read, Write};
+use std::path::Path;
 
 #[derive(Debug)]
 struct Pic {
@@ -24,43 +26,46 @@ impl Pic {
 }
 
 pub fn download(params: &Params) {
-    let resolution = params
-        .resolution
-        .clone()
-        .expect("Resolutions are wrong");
+    let resolution = params.resolution.clone().expect("Resolutions are wrong");
     let empty_dir = params.download_empty;
     let sfw = params.download_sfw;
 
     let dir = Path::new(&params.dir);
     if empty_dir {
         //del previous files or dirs
-        for entry in dir.read_dir().expect(&format!("Directory {} can't read", params.dir)) {
+        for entry in dir
+            .read_dir()
+            .expect(&format!("Directory {} can't read", params.dir))
+        {
             if let Ok(entry) = entry {
                 match entry.file_type() {
                     Ok(t) => {
                         if t.is_dir() {
-                            if let Err(e) = remove_dir_all(entry.path()){
-                                if e.kind() != std::io::ErrorKind::Other && 
-                                e.kind() != std::io::ErrorKind::NotFound
+                            if let Err(e) = remove_dir_all(entry.path()) {
+                                if e.kind() != std::io::ErrorKind::Other
+                                    && e.kind() != std::io::ErrorKind::NotFound
                                 {
-                                    fatal!("Directory {} is failed remove:{:?}",&params.dir, e);
+                                    fatal!("Directory {} is failed remove:{:?}", &params.dir, e);
                                 }
                             }
                         } else {
-                            if let Err(e) = remove_file(entry.path()){
-                                if e.kind() != std::io::ErrorKind::Other &&
-                                e.kind() != std::io::ErrorKind::NotFound
+                            if let Err(e) = remove_file(entry.path()) {
+                                if e.kind() != std::io::ErrorKind::Other
+                                    && e.kind() != std::io::ErrorKind::NotFound
                                 {
-                                    fatal!("File {} is failed remove:{:?}",entry.path().to_string_lossy(), e);
+                                    fatal!(
+                                        "File {} is failed remove:{:?}",
+                                        entry.path().to_string_lossy(),
+                                        e
+                                    );
                                 }
                             }
                         }
-                    },
+                    }
                     Err(e) => {
                         fatal!("Get file types error:{:?}", e);
                     }
                 }
-
             }
         }
     }
@@ -69,26 +74,32 @@ pub fn download(params: &Params) {
     for i in resolution.iter() {
         let pic_dir = format!("{}{}", params.dir, i);
         match create_dir(&pic_dir) {
-            Ok(()) => {},
-            Err(e) => {
-                match e.kind() {
-                    std::io::ErrorKind::AlreadyExists => {},
-                    _ => {fatal!("Couldn't create dir {}:{:?}", pic_dir, e);},
+            Ok(()) => {}
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::AlreadyExists => {}
+                _ => {
+                    fatal!("Couldn't create dir {}:{:?}", pic_dir, e);
                 }
-            }
+            },
         }
     }
 
-    let mut pics:HashMap<String, Vec<Pic>> = HashMap::new();
-    let mut pic_count:usize = 0;
+    let mut pics: HashMap<String, Vec<Pic>> = HashMap::new();
+    let mut pic_count: usize = 0;
     //get pictures
     println!("Starting...");
+    let wallpapers: Vec<Box<dyn Wallpaper>> = vec![Box::new(Wstock), Box::new(Wallhaven)];
     for i in resolution.iter() {
-        match get_pics(i, sfw, &params.proxy) {
+        let index = rand::thread_rng().gen_range(0, wallpapers.len());
+        match wallpapers
+            .get(index)
+            .unwrap()
+            .get_pics(i, sfw, &params.proxy)
+        {
             Ok(ret) => {
                 pic_count += ret.len();
                 pics.insert(i.to_owned(), ret);
-            },
+            }
             Err(e) => panic!("Main error {}", e),
         }
     }
@@ -108,14 +119,14 @@ fn save_pics(pics: &Vec<Pic>, pic_dir: &str) {
         let mut file = match File::create(path) {
             Err(why) => {
                 fatal!("Couldn't create {}: {}", file_name, why.to_string());
-            },
+            }
             Ok(file) => file,
         };
 
         match file.write_all(&pic.body) {
             Err(why) => {
                 fatal!("Couldn't write to {}:{}", file_name, why.to_string());
-            },
+            }
             Ok(_) => {}
         }
     }
@@ -128,7 +139,7 @@ struct Singleton {
 }
 
 impl Singleton {
-    fn new(proxy: & Option<String>) -> &Singleton {
+    fn new(proxy: &Option<String>) -> &Singleton {
         unsafe {
             match &SINGLETON_HTTP_CLIENT {
                 Some(r) => r,
@@ -138,74 +149,165 @@ impl Singleton {
                         client = client.proxy(reqwest::Proxy::http(p).unwrap());
                     }
                     let client = client.build().unwrap();
-                    SINGLETON_HTTP_CLIENT = Some(Singleton{v: client});
+                    SINGLETON_HTTP_CLIENT = Some(Singleton { v: client });
                     Singleton::new(proxy)
-                },
+                }
             }
         }
     }
 }
 
-fn get_pics(resolution: &str, sfw: bool, proxy:& Option<String>) -> Result<Vec<Pic>, Box<dyn Error>> {
-    let mut category = 111;
-    let mut purity = 110;
-    if sfw {
-        category = 110;
-        purity = 100;
-    }
+struct Wallhaven;
 
-    let client = &Singleton::new(proxy).v;
-    let url = format!(
-        "{}{}{}{}{}{}{}",
-        "https://wallhaven.cc/search?categories=",
-        category,
-        "&purity=",
-        purity,
-        "&atleast=",
-        resolution,
-        "&sorting=random&order=desc"
-    );
-    let mut res = client.get(&url).send().expect("an error");
-    let mut body = "".to_string();
+struct Wstock;
 
-    res.read_to_string(&mut body)?;
-
-    let re = Regex::new("class=\"preview\"\\s+href=\"(.*?)\"")?;
-    let mut pics = Vec::new();
-    for caps in re.captures_iter(&body) {
-        match get_pic_from_detail_page_url(&caps[1], proxy) {
-            Ok(pic) => {
-                pics.push(pic);
-            }
-            Err(e) => {
-                println!("{}", e);
-            }
-        }
-    }
-
-    Ok(pics)
+trait Wallpaper {
+    fn get_pics(
+        &self,
+        resolution: &str,
+        sfw: bool,
+        proxy: &Option<String>,
+    ) -> Result<Vec<Pic>, Box<dyn Error>>;
 }
 
-fn get_pic_from_detail_page_url(url: &str, proxy:& Option<String>) -> Result<Pic, Box<dyn Error>> {
-    let client = &Singleton::new(proxy).v;
-    let mut res = client.get(url).send()?;
-    let mut body = "".to_string();
+impl Wallhaven {
+    fn get_pic_from_detail_page_url(
+        &self,
+        url: &str,
+        proxy: &Option<String>,
+    ) -> Result<Pic, Box<dyn Error>> {
+        let client = &Singleton::new(proxy).v;
+        let mut res = client.get(url).send()?;
+        let mut body = "".to_string();
 
-    res.read_to_string(&mut body)?;
+        res.read_to_string(&mut body)?;
 
-    let re = Regex::new("id=\"wallpaper\" src=\"(.*?)\"")?;
-    let mut full_pic_url = "".to_string();
-    for caps in re.captures_iter(&body) {
-        full_pic_url = caps[1].to_string();
+        let re = Regex::new("id=\"wallpaper\" src=\"(.*?)\"")?;
+        let mut full_pic_url = "".to_string();
+        for caps in re.captures_iter(&body) {
+            full_pic_url = caps[1].to_string();
+        }
+
+        let filename = get_basename(&full_pic_url);
+
+        let mut res = client.get(&full_pic_url).send()?;
+        let mut body = Vec::new();
+        res.read_to_end(&mut body)?;
+
+        Ok(Pic::new(filename, body))
     }
+}
 
-    let filename = get_basename(&full_pic_url);
+impl Wstock {
+    fn get_pic_from_detail_page_url(
+        &self,
+        url: &str,
+        proxy: &Option<String>,
+    ) -> Result<Pic, Box<dyn Error>> {
+        let client = &Singleton::new(proxy).v;
 
-    let mut res = client.get(&full_pic_url).send()?;
-    let mut body = Vec::new();
-    res.read_to_end(&mut body)?;
+        let filename = get_basename(url);
+        let mut res = client.get(url).send()?;
+        let mut body = Vec::new();
+        res.read_to_end(&mut body)?;
 
-    Ok(Pic::new(filename, body))
+        Ok(Pic::new(filename, body))
+    }
+}
+
+impl Wallpaper for Wallhaven {
+    fn get_pics(
+        &self,
+        resolution: &str,
+        sfw: bool,
+        proxy: &Option<String>,
+    ) -> Result<Vec<Pic>, Box<dyn Error>> {
+        let mut category = 111;
+        let mut purity = 110;
+        if sfw {
+            category = 110;
+            purity = 100;
+        }
+
+        let client = &Singleton::new(proxy).v;
+        let url = format!(
+            "{}{}{}{}{}{}{}",
+            "https://wallhaven.cc/search?categories=",
+            category,
+            "&purity=",
+            purity,
+            "&atleast=",
+            resolution,
+            "&sorting=random&order=desc"
+        );
+        let mut res = client.get(&url).send().expect("an error");
+        let mut body = "".to_string();
+
+        res.read_to_string(&mut body)?;
+
+        let re = Regex::new("class=\"preview\"\\s+href=\"(.*?)\"")?;
+        let mut pics = Vec::new();
+        for caps in re.captures_iter(&body) {
+            match self.get_pic_from_detail_page_url(&caps[1], proxy) {
+                Ok(pic) => {
+                    pics.push(pic);
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
+        }
+
+        Ok(pics)
+    }
+}
+
+impl Wallpaper for Wstock {
+    fn get_pics(
+        &self,
+        resolution: &str,
+        _sfw: bool,
+        proxy: &Option<String>,
+    ) -> Result<Vec<Pic>, Box<dyn Error>> {
+        let client = &Singleton::new(proxy).v;
+        let prefix_url = "https://wallpaperstock.net";
+        let url = format!("{}/wallpapers_{}r.html", prefix_url, resolution,);
+        let mut res = client.get(&url).send().expect("an error");
+        let mut body = "".to_string();
+        res.read_to_string(&mut body)?;
+        let re = Regex::new("class=\"pagination\">.*?a>\\.\\.\\.<a.*?>(\\d+)</")?;
+        let max_page_match = (&re.captures_iter(&body).next().unwrap()[1])
+            .parse::<u32>()
+            .unwrap();
+
+        let random_page = rand::thread_rng().gen_range(1, max_page_match);
+        let url = format!(
+            "{}/wallpapers_p{}_{}r.html",
+            prefix_url, random_page, resolution,
+        );
+        let mut res = client.get(&url).send().expect("an error");
+        let mut body = "".to_string();
+        res.read_to_string(&mut body)?;
+
+        let re = Regex::new("class=\"links\">[\\s\\S]*?href='(.*?)'")?;
+
+        let mut pics = Vec::new();
+        for caps in re.captures_iter(&body) {
+            let new_path = &caps[1]
+                .replace("-wallpapers_w", "_wallpapers_")
+                .replace(".html", &format!("_{}{}", resolution, ".jpg"));
+            match self.get_pic_from_detail_page_url(&format!("{}{}", prefix_url, new_path), proxy) {
+                Ok(pic) => {
+                    pics.push(pic);
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
+        }
+
+        Ok(pics)
+    }
 }
 
 fn get_basename(url: &str) -> String {
